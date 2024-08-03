5702783b05271b9c4984b56322798e9ef2953db9
maryam <myjm821@icloud.com>
2024-08-03 15:29:22 +0300

    hh

---
+++ little-lemon/src/Header.js
@@ -0,0 +1,12 @@
+// Header.js
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <img src="./little-lemon-logo.png" alt="Little Lemon Logo" className="logo" />
+    </header>
+  );
+}
+
+export default Header;
---
+++ little-lemon/src/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <main>
+      {/* Content for main */}
+    </main>
+  );
+}
+
+export default Main;
